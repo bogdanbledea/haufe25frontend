ac2f72893e8991742cc91fa9850c5cf585798c73
Alexandru Chiriac <tagestudio.alexandru@gmail.com>
2025-08-21 11:04:27 +0300

    TEST

--- src/Widget2.tsx
+++ src/Widget2.tsx
@@ -1,13 +1,13 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
 import axios from "axios";
 
-axios
-  .get(
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=3"
-  )
-  .then((response) => {
-    console.log(response.data);
-  });
+const threeDaysForecastApi = (latitude: number, longitude: number) => {
+  return `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=3`;
+};
+
+axios.get(threeDaysForecastApi(45.75372, 21.22571)).then((response) => {
+  console.log(response.data);
+});
 
 const Widget2 = () => {
   return (

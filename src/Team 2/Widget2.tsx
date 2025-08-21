8f44c1f5970c167243069b645a7177b125dee1c7
Alexandru Chiriac <tagestudio.alexandru@gmail.com>
2025-08-21 10:53:48 +0300

    Auto stash before merge of "Team2StartHere" and "origin/Team2StartHere"

--- src/Team 2/Widget2.tsx
+++ src/Team 2/Widget2.tsx
@@ -1,4 +1,13 @@
-import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card.tsx";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import axios from "axios";
+
+axios
+  .get(
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=3"
+  )
+  .then((response) => {
+    console.log(response.data);
+  });
 
 const Widget2 = () => {
   return (
@@ -7,6 +16,7 @@ const Widget2 = () => {
         <CardTitle>3-Day Forecast Chart</CardTitle>
       </CardHeader>
       <CardContent className="flex items-center justify-center h-full">
+        <p className="text-gray-500">We are starting here!</p>
       </CardContent>
     </Card>
   );

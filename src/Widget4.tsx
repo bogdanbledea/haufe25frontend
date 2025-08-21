650053b832395c43e8dc87460228658105fe7c5e
Ionut180205 <ionut.cioarsa@gmail.com>
2025-08-21 11:20:53 +0300

    city info

--- src/Widget4.tsx
+++ src/Widget4.tsx
@@ -1,16 +1,34 @@
+import { useAppContext } from "./AppContext";
 import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
 
 const Widget4 = () => {
-  return (
-    <Card className="w-full h-full">
-      <CardHeader>
-        <CardTitle>City Info</CardTitle>
-      </CardHeader>
-      <CardContent className="flex items-center justify-center h-full">
-        <p className="text-gray-500">Team 4: Start here</p>
-      </CardContent>
-    </Card>
-  );
+  const { selectedCity } = useAppContext();
+  if (selectedCity)
+    return (
+      <Card className="w-full h-full">
+        <CardHeader>
+          <CardTitle>City Info</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center justify-center h-full space-y-2">
+          <p className="text-gray-500">
+            Selected country: {selectedCity.country}
+          </p>
+          <p className="text-gray-500">Latitude: {selectedCity.latitude}</p>
+          <p className="text-gray-500">Longitude: {selectedCity.longitude}</p>
+        </CardContent>
+      </Card>
+    );
+  else
+    return (
+      <Card className="w-full h-full">
+        <CardHeader>
+          <CardTitle>City Info</CardTitle>
+        </CardHeader>
+        <CardContent className="flex items-center justify-center h-full">
+          <p className="text-gray-500">Team 4: Start here</p>
+        </CardContent>
+      </Card>
+    );
 };
 
 export default Widget4;
